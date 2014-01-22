c10f68e33cfccd3f87684cc8d0660abbc93ae598
Daewook Kim <daewookkim@nitrous-oxide.csail.mit.edu>
2014-01-22 12:41:13 -0500

    added script files in bin folder

---
+++ bin/run_valgrind.sh
@@ -0,0 +1,1 @@
+valgrind --tool=callgrind ./TAppEncoderStatic -c ../cfg/encoder_randomaccess_main.cfg -wdt 352 -hgt 288 -q 0 -fr 25 -f 50 -i ../../videos/bus_cif.yuv -b bus_cif.hevc -o HM_12_0_bus_cif.yuv
