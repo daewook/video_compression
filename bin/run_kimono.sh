acfc2e9dbfd15f29c3f526787e7dd500da0e08a7
Daewook Kim <daewookkim@ether.csail.mit.edu>
2015-04-27 15:45:37 -0400

    changed compiler to icpc, and added multiple scripts to encode videos

---
+++ bin/run_kimono.sh
@@ -0,0 +1,2 @@
+./TAppEncoderStatic -c ../cfg/encoder_randomaccess_main.cfg -wdt 1920 -hgt 1080 -q 0 -fr 24 -f 10 --SEIDecodedPictureHash=1 -i ../../videos/Kimono1_1920x1080_24.yuv -b Kimono.hevc -o HM_12_0_kimono.yuv 
+#./TAppDecoderStatic -b Kimono.hevc -o HM_12_0_kimono.yuv
