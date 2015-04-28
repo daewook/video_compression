af4d766c0c7b99c7b27aa3189bbab1f70e1a06b8
Daewook Kim <daewookkim@ether.csail.mit.edu>
2015-04-28 10:04:13 -0400

    modified number of frames to 60

--- bin/run_tile_kimono.sh
+++ bin/run_tile_kimono.sh
@@ -1,2 +1,2 @@
-./TAppEncoderStatic -c ../cfg/encoder_randomaccess_tile_kimono.cfg -wdt 1920 -hgt 1080 -fr 24 -f 10 --SEIDecodedPictureHash=1 -i ../../videos/Kimono1_1920x1080_24.yuv -b Kimono.hevc -o HM_12_0_kimono.yuv 
+./TAppEncoderStatic -c ../cfg/encoder_randomaccess_tile_kimono.cfg -wdt 1920 -hgt 1080 -fr 24 -f 60 --SEIDecodedPictureHash=1 -i ../../videos/Kimono1_1920x1080_24.yuv -b Kimono.hevc -o HM_12_0_kimono.yuv 
 #./TAppDecoderStatic -b Kimono.hevc -o HM_12_0_kimono.yuv
