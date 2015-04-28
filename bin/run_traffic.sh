fcf017f7346a45c38c0393eac4a861d7d2ba1aa3
Daewook Kim <daewookkim@ether.csail.mit.edu>
2015-04-27 21:08:35 -0400

    removed -q 0 from scripts

--- bin/run_traffic.sh
+++ bin/run_traffic.sh
@@ -1,2 +1,2 @@
-./TAppEncoderStatic -c ../cfg/encoder_randomaccess_main.cfg -wdt 2560 -hgt 1600 -q 0 -fr 30 -f 60 --SEIDecodedPictureHash=1 -i ../../videos/Traffic_2560x1600_30_crop.yuv -b Traffic.hevc -o HM_12_0_Traffic.yuv 
+./TAppEncoderStatic -c ../cfg/encoder_randomaccess_main.cfg -wdt 2560 -hgt 1600 -fr 30 -f 60 --SEIDecodedPictureHash=1 -i ../../videos/Traffic_2560x1600_30_crop.yuv -b Traffic.hevc -o HM_12_0_Traffic.yuv 
 #./TAppDecoderStatic -b Traffic.hevc -o HM_12_0_Traffic.yuv
